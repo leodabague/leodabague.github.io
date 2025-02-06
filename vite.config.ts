e5043fd9d0a0fa022679ac73e71b889b291b8c22
Leo Dabague <dabague88@gmail.com>
2025-02-06 11:46:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/llm-hardware/'
+  base: '/',  // Altere para '/' ou seu repositório se estiver em subdiretório
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html',
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        entryFileNames: 'assets/[name].[hash].js',
+      }
+    }
+  }
 })
